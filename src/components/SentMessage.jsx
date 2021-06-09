9162db0d50e49c7c1c57caca9e4ed4cfa32cc85f
tanu31195 <11332345+tanu31195@users.noreply.github.com>
2021-06-10 00:00:27 +0530

    Fixed image attachment issue

--- src/components/SentMessage.jsx
+++ src/components/SentMessage.jsx
@@ -2,7 +2,7 @@ const SentMessage = ({ message }) => {
     if (message?.attachments?.length > 0) {
         return (
             <img
-                src={message.attachement[0].file}
+                src={message.attachments[0].file}
                 alt="message-attachment"
                 className="message-image"
                 style={{ float: 'right' }}
