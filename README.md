e2bc31a09c41bea1cb5c7d29c7ac886987dfba95
tanu31195 <11332345+tanu31195@users.noreply.github.com>
2021-06-13 23:27:05 +0530

    Added logout

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
+[![Netlify Status](https://api.netlify.com/api/v1/badges/29f57c12-e542-4251-acc5-f53ab99424f4/deploy-status)](https://app.netlify.com/sites/tanu31195-react-chat-app/deploys)
+
+Created using [chatengine.io](https://chatengine.io/)
+
 `npx create-react-app ./`
 
 `npm install @ant-design/icons axios react-chat-engine`
@@ -9,3 +13,7 @@
 `git push -u origin main`
 
 `npm run build`
+
+TODO
+Add github actions
+Add is typing
--- src/App.js
+++ src/App.js
@@ -8,14 +8,25 @@ import LoginForm from './components/LoginForm';
 const App = () => {
 	if (!localStorage.getItem('username')) return <LoginForm />
 
+	const handleLogout = () => {
+		localStorage.setItem('username', '');
+		localStorage.setItem('password', '');
+		window.location.reload();
+	}
+
 	return (
-		<ChatEngine
-			height='100vh'
-			projectID={process.env.REACT_APP_CHAT_ENGINE_PROJECT_ID}
-			userName={localStorage.getItem('username')}
-			userSecret={localStorage.getItem('password')}
-			renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} />}
-		/>
+		<>
+			<div style={{height: '2vh'}}>
+				<button style={{position: 'absolute', right: 0}} onClick={handleLogout}>Logout</button>
+			</div>
+			<ChatEngine
+				height='98vh'
+				projectID={process.env.REACT_APP_CHAT_ENGINE_PROJECT_ID}
+				userName={localStorage.getItem('username')}
+				userSecret={localStorage.getItem('password')}
+				renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} />}
+			/>
+		</>
 	);
 }
 
